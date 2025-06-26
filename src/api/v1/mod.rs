791af16a78b0d316899c5809c722c7c85959f41f
Adam Jasniewicz <adam@Adams-MacBook-Pro.local>
2025-06-26 17:35:02 -0400

    added workflow and activity queue

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -13,4 +13,6 @@ pub fn router() -> Router<ImmortalService> {
         .route("/run/activity", post(run::run_activity))
         .route("/history", get(server::get_history))
         .route("/workers", get(server::get_workers))
+        .route("/workflow-queue", get(server::get_workflow_queue))
+        .route("/activity-queue", get(server::get_activity_queue))
 }
